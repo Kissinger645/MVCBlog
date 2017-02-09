08cbaa5d7f7eb86ee72428944f1626cb8b21f63e
Bobby <kissinger645@gmail.com>
2017-02-08 22:51:55 -0500

    Added blog.  Getting errors updating database...Error Number:-1,State:0,Class:20

---
+++ MVCBlog/Migrations/201702090347541_initial.cs
@@ -0,0 +1,30 @@
+namespace MVCBlog.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.BlogPosts",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Author = c.String(),
+                        TeaserText = c.String(),
+                        Body = c.String(),
+                        Created = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.BlogPosts");
+        }
+    }
+}
---
+++ MVCBlog/Models/BlogPost.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace MVCBlog.Models
+{    
+        public class BlogPost
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public string Author { get; set; }
+            public string TeaserText { get; set; }
+            public string Body { get; set; }
+            public DateTime Created { get; set; }
+            
+        }
+
+        public class BlogPostContext : DbContext
+        {
+            public DbSet<BlogPost> BlogPosts { get; set; }
+            
+        }    
+}
